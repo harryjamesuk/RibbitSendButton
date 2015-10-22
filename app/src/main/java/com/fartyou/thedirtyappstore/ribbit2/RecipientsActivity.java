8297447a4e4665b83df0fb442e6d343ce7dd3f7d
Harry James <harryjamesuk@users.noreply.github.com>
2015-10-22 21:14:54 +0100

    Added send button to Action Bar
    Added the send buttons to project, added menu_recipients, removed
    action_send from menu_main, updated strings.
    
    For some strange reason, when we use the delegated Toolbar, Android
    doesn't like app:showAsAction but works fine with android:showAsAction
    (This will show as an error in Android Studio, but the code will still
    compile fine, however).
    
    Anyhow, use the onCreateOptionsMenu() in RecipientsActivity.java to
    inflate the ActionBar or the Toolbar (Works for both) with the send
    icon.

--- app/src/main/java/com/fartyou/thedirtyappstore/ribbit2/RecipientsActivity.java
+++ app/src/main/java/com/fartyou/thedirtyappstore/ribbit2/RecipientsActivity.java
@@ -106,4 +106,11 @@ public class RecipientsActivity extends ListActivity {
             }
         });
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_recipients, menu);
+        return true;
+    }
 }
