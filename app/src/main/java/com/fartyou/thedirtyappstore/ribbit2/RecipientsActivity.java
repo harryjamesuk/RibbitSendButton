e1cf31d4a0c60345e101cee4afed092f294af51a
Harry James <harryjamesuk@users.noreply.github.com>
2015-10-22 20:32:29 +0100

    Solution 2: Add the new Toolbar!
    ActionBar's aren't recommended for use anymore so if you want your app
    to be up-to-date, merge this commit!
    
    Important: If you merge this commit, there may be some things that are
    slightly different to using ActionBar's, like in the video. Therefore,
    if you run into any problems on the way, let someone know on the forums
    or post a comment on this GitHub and I'll get back to you.
    
    There is quite a bit of code required to do this since our normal
    setSupportActionBar() method isn't part of ListActivity but for now, all
    you need to know is that it works! Don't try to understand it just yet
    :)

--- app/src/main/java/com/fartyou/thedirtyappstore/ribbit2/RecipientsActivity.java
+++ app/src/main/java/com/fartyou/thedirtyappstore/ribbit2/RecipientsActivity.java
@@ -2,8 +2,13 @@ package com.fartyou.thedirtyappstore.ribbit2;
 
 import android.app.AlertDialog;
 import android.app.ListActivity;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatCallback;
+import android.support.v7.app.AppCompatDelegate;
+import android.support.v7.view.ActionMode;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -34,6 +39,29 @@ public class RecipientsActivity extends ListActivity {
         requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
         setContentView(R.layout.activity_recipients);
 
+        // Code for the Toolbar:
+
+        AppCompatDelegate delegate = AppCompatDelegate.create(this, new AppCompatCallback() {
+            @Override
+            public void onSupportActionModeStarted(ActionMode mode) {}
+
+            @Override
+            public void onSupportActionModeFinished(ActionMode mode) {}
+
+            @Nullable
+            @Override
+            public ActionMode onWindowStartingSupportActionMode(ActionMode.Callback callback) {
+                return null;
+            }
+        });
+
+        delegate.onCreate(savedInstanceState);
+        delegate.setContentView(R.layout.activity_recipients);
+
+        Toolbar toolbar= (Toolbar) findViewById(R.id.toolbar);
+        delegate.setSupportActionBar(toolbar);
+
+        // End of Toolbar code.
 
         getListView().setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
 
